908479b2dd78108c75538dd662483eabd9f5afbb
George Higbie <gehigbie@gmail.com>
2018-02-28 16:56:57 +0000

    Set up map function

--- src/BlinkerApp.js
+++ src/BlinkerApp.js
@@ -10,12 +10,17 @@ import Footer from './components/Footer/Footer';
 import CarTile from './components/CarTile/CarTile';
 
 class BlinkerApp extends Component {
+  state = {
+    carList: []
+  }
+  
   title = "Car Finder";
   subtitle ="Find the right car for you!"
   
   componentDidMount(){
     axios.get('https://gist.githubusercontent.com/creatifyme/2a334c00a117097bfdb47f031edf292c/raw/efb52ecf1cf92e2261f504ec7639c68b5ff390bd/cars.json')
     .then(response => {
+      this.setState({carList: response.data})
       console.log(response);
     });
   }
@@ -25,7 +30,7 @@ class BlinkerApp extends Component {
       <div>
         <Header title={this.title}
                 subtitle={this.subtitle} />
-        <Display />
+        <Display carList={this.state.carList} />
         <Footer />
       </div>
     );
--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -4,17 +4,16 @@ import './Display.css';
 import CarTile from './../CarTile/CarTile';
 
 class Display extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            stuff: [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-        }
-    }
 
     render(){
         return(
             <div className="container flex">
-                {this.state.stuff.map((car) => <CarTile />)}
+                {this.props.carList.map((car) => 
+                    (<CarTile key={car.image_url}
+                              make={car.make}
+                              model={car.model}
+                              millege={car.millege}
+                              image={this.props.image_url}/>))}
             </div>
         );
     }
