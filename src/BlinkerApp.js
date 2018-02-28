38eee50f05ef29877c1ada8605e5f98130310e0d
George Higbie <gehigbie@gmail.com>
2018-02-28 02:41:46 +0000

    Change import paths

--- src/BlinkerApp.js
+++ src/BlinkerApp.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './BlinkerApp.css';
 
-import Header from './components/Header';
-import SearchBar from '/components/SearchBar'
-import Display from './components/Display';
-import Footer from './components/Footer';
-import CarTile from './components/CarTile';
+import Header from './components/Header/Header';
+import SearchBar from '/components/SearchBar/SearchBar'
+import Display from './components/Display/Display';
+import Footer from './components/Footer/Footer';
+import CarTile from './components/CarTile/CarTile';
 
 class BlinkerApp extends Component {
   title = "Car Finder";
