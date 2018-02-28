a7c27c9ea9f257e9a2172f192f7bc6e85ec46120
ghigbie <gehigbie@gmail.com>
2018-02-27 19:57:17 -0700

    Change class to class name for cards

--- src/components/CarTile/CarTile.js
+++ src/components/CarTile/CarTile.js
@@ -16,28 +16,28 @@ class CarTile extends Component{
                 </div>
             </div>
             
-            <div class="card" style="width: 20rem;">
-              <img class="card-img-top" 
+            <div className="card" style="width: 20rem;">
+              <img className="card-img-top" 
                    src="https://images.pexels.com/photos/8700/wall-animal-dog-pet.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb" 
                    alt="Card image cap"/>
-              <div class="card-block">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div className="card-block">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
             </div>
             
-            <div class="card card-inverse">
-              <img class="card-img" 
+            <div className="card card-inverse">
+              <img className="card-img" 
                     src="https://images.pexels.com/photos/8700/wall-animal-dog-pet.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb" 
                     alt="Card image" />
-              <div class="card-img-overlay">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-              <div class="card-block">
-                <h4 class="card-title">Card title</h4>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <div className="card-img-overlay">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <div className="card-block">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
               </div>
               </div>
