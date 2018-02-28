367b897945eef4604a625385473be4f3fee23ae5
George Higbie <gehigbie@gmail.com>
2018-02-28 16:41:18 +0000

    Change CarTile class to callName

--- src/components/CarTile/CarTile.js
+++ src/components/CarTile/CarTile.js
@@ -11,7 +11,7 @@ class CarTile extends Component{
               <div className="card-block container details">
                 <h4 className="card-title">Card title</h4>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-outline-primary">Go somewhere</a>
+                <a href="#" className="btn btn-outline-primary">Go somewhere</a>
               </div>
             </div>
         );
