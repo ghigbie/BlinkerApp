91c8569a14993d6fd68c08e308753c83904e21ff
George Higbie <gehigbie@gmail.com>
2018-02-28 16:38:24 +0000

    Modify header class

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = (props) => {
 
     return(
-        <div class="header">
+        <div className="header">
             <div className="container">
                 <h1>{props.title}</h1>
                 <h3>{props.subtitle && props.subtitle}</h3>
