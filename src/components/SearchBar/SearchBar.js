3adfe0d075db1a012ba6f1c29ff587364cf3f68e
George Higbie <gehigbie@gmail.com>
2018-03-01 14:32:58 +0000

    Add inline styles

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react';
 import './SearchBar.css';
 
 class SearchBar extends Component{
+    style = {
+        width: '80%',
+        marginLeft: '10%'
+    };
+    
     render(){
         return(
             <input type="text" 
                    className="form-control"
+                   style={this.style}
                    id="search-bar"
                    placeholder="Search for a car..." />
         );
