035dda2ebe4f95d4595fb20d3fcca0cf142b2c06
George Higbie <gehigbie@gmail.com>
2018-03-01 14:26:49 +0000

    Add content to search bar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import './SearchBar.css';
+
+class SearchBar extends Component{
+    render(){
+        return(
+            <input type="text" 
+                   className="form-control"
+                   id="search-bar"
+                   placeholder="Search for a car..." />
+        );
+    }
+}
