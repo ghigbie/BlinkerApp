42430452d15dcba876d3f9cb684959f74bf10508
ghigbie <gehigbie@gmail.com>
2018-03-02 20:36:45 -0700

    Change class to className on modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -8,7 +8,7 @@ const Modal = (props) => {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title" id="exampleModalLongTitle">{`${props.year} ${props.make} ${props.model}`}</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
