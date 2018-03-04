e7447a16c0cddbf224ed3ead0e3db42ce6b9d783
George Higbie <gehigbie@gmail.com>
2018-03-04 22:06:06 +0000

    Add styles to sorting buttons

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -30,8 +30,8 @@ class Display extends Component{
     }
 
     render(props){
-        this.sortingMethod();
-        const cars = this.state.carList.map((car, index) => 
+        let filteredCars = this.state.carList;
+        const cars = filteredCars.map((car, index) => 
                     (<CarTile key={index}
                               uniqueID={index}
                               year={car.year}
@@ -46,6 +46,11 @@ class Display extends Component{
         return(
             <div className="container flex">
                 <SearchBar />
+                <div className="container flex flex-centered">
+                    <button className="btn btn-outline-primary sorting">Sort By Year</button>
+                    <button className="btn btn-outline-primary sorting">Sort By Mileage</button>
+                    <button className="btn btn-outline-primary sorting">Sort By Listing Date</button>
+                </div>
                 {this.state.errorMessage? error : cars}
             </div>
         );
